f540b181a23ac587fd58e5d46ac3124e83f19504
Brian McCarthy <brian.mcc@rthy.org>
2014-12-24 15:14:38 -0500

    updated to use iframe for modal

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,22 +1,134 @@
 'use strict';
 
+var isActive = false,
+  showPreview = true,
+  isLinksActive = true,
+  currentProject = null,
+  projects = [];
+
 chrome.runtime.onInstalled.addListener(function (details) {
   console.log('previousVersion', details.previousVersion);
 });
 
-var updateBadge = function (isActive){
+var updateBadge = function (isActive) {
   chrome.browserAction.setBadgeText({text: isActive ? "on" : "off"});
 };
 
-chrome.storage.sync.get("isActive", function(data){
-  updateBadge(data.isActive);
-
-  chrome.storage.onChanged.addListener(function (changes) {
-    for (var key in changes) {
-      if( key === "isActive"){
-        var storageChange = changes[key];
-        updateBadge(storageChange.newValue);
-      }
+var sendToEachTab = function (message) {
+  chrome.tabs.query({}, function (tabs) {
+    for (var i = 0; i < tabs.length; ++i) {
+      chrome.tabs.sendMessage(tabs[i].id, message);
     }
   });
+};
+
+var onIsActiveChanged = function (active) {
+  isActive = active;
+  updateBadge(active);
+
+  if (active) {
+    sendToEachTab({type: "activate", project: currentProject});
+  } else {
+    sendToEachTab({type: "deactivate"});
+  }
+};
+
+var onCurrentProjectChanged = function (currentProjectId) {
+  var found = false;
+
+  for (var i = 0; i < projects.length; i++) {
+    if (projects[i].id === currentProjectId) {
+      currentProject = projects[i];
+      found = true;
+      break;
+    }
+  }
+
+  if (found === false) {
+    currentProject = null;
+  }
+
+  sendToEachTab({type: "project_changed", project: currentProject});
+};
+
+var onIsLinksActiveChanged = function (linksActive) {
+  isLinksActive = linksActive;
+  sendToEachTab({type: "toggle_links_active", isLinksActive: isLinksActive});
+};
+
+var onShowPreviewChanged = function (sp) {
+  showPreview = sp;
+  sendToEachTab({type: "toggle_show_preview", showPreview: sp});
+};
+
+function handleMessage(request, sender, sendResponse){
+  console.log("Message received in background: " + JSON.stringify(request));
+}
+
+chrome.runtime.onMessage.addListener(handleMessage);
+
+chrome.storage.sync.get("isActive", function (data) {
+  onIsActiveChanged(data.isActive === true);
+});
+
+chrome.storage.sync.get("showPreview", function (data) {
+  onShowPreviewChanged(data.showPreview == null || data.showPreview === true);
+});
+
+chrome.storage.sync.get("isLinksActive", function (data) {
+  onIsLinksActiveChanged(data.isLinksActive == null || data.isLinksActive);
+});
+
+chrome.storage.sync.get("projects", function (projectsData) {
+  if (projectsData.projects == null) {
+    projects = [];
+    currentProject = null;
+    return;
+  }
+
+  projects = projectsData.projects || [];
+
+  chrome.storage.sync.get("currentProjectId", function (data) {
+    var currentProjectId = data.currentProjectId;
+
+    if (currentProjectId == null) {
+      currentProject = null;
+      return;
+    }
+
+    for (var i = 0; i < projectsData.projects.length; i++) {
+      if (projectsData.projects[i].id === currentProjectId) {
+        console.log("setting project: " + JSON.stringify(projectsData.projects[i]));
+        currentProject = projectsData.projects[i];
+        return;
+      }
+    }
+
+    console.log("setting project: null");
+    currentProject = null;
+  });
+});
+
+chrome.storage.onChanged.addListener(function (changes) {
+  for (var key in changes) {
+    switch (key) {
+      case "isActive":
+        onIsActiveChanged(changes[key].newValue);
+        break;
+      case "showPreview":
+        onShowPreviewChanged(changes[key].newValue);
+        break;
+      case  "projects":
+        break;
+      case "isLinksActive":
+        onIsLinksActiveChanged(changes[key].newValue);
+        break;
+      case "currentProjectId":
+        onCurrentProjectChanged(changes[key].newValue);
+        break;
+      default:
+        console.log("unknown storage item changed: " + JSON.stringify(changes));
+        break;
+    }
+  }
 });
--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,186 +1,240 @@
-(function () {
-  'use strict';
+'use strict';
 
-  var hasBeenInit = false;
-  var allTagAppliers = {};
+var hasBeenInit = false,
+  allTagAppliers = {},
+  currentProject,
+  currentTag;
 
-  var isActive, showPreview, isLinksActive;
-  var currentProject;
+var extensionUrl = chrome.extension.getURL("");
+if (extensionUrl.charAt(extensionUrl.length - 1) === '/') {
+  extensionUrl = extensionUrl.slice(0, -1);
+}
 
-  try {
-    rangy.init();
+var updateStyles = function () {
+  if (hasBeenInit === false) {
+    return;
   }
-  catch(e){
-    console.log(e);
+
+  var tags = [];
+
+  if (currentProject != null && currentProject.tags != null) {
+    tags = currentProject.tags;
   }
 
-  var updateTags = function () {
-    // after updating the modal dialog options, add the css styles to the page
-    var sheet = document.getElementById("web-annotator-styles");
-    if (sheet) {
-      sheet.parentNode.removeChild(sheet);
-    }
+  // after updating the modal dialog options, add the css styles to the page
+  var sheet = document.getElementById("web-annotator-styles");
+  if (sheet) {
+    sheet.parentNode.removeChild(sheet);
+  }
 
-    sheet = (function () {
-      var style = document.createElement("style");
-      style.setAttribute("media", "screen");
-      style.setAttribute("id", "web-annotator-styles");
+  sheet = (function () {
+    var style = document.createElement("style");
+    style.setAttribute("media", "screen");
+    style.setAttribute("id", "web-annotator-styles");
 
-      document.head.appendChild(style);
-      return style.sheet;
-    })();
+    document.head.appendChild(style);
+    return style.sheet;
+  })();
 
-    var options = "";
-    allTagAppliers = {};
+  allTagAppliers = {};
 
-    if (currentProject != null) {
-      $.each(currentProject.tags, function (i, item) {
-        options += "<option>" + item.name + "</option>";
+  $.each(tags, function (i, item) {
 
-        sheet.insertRule("web-annotator[tag='" + item.name + "'] { background-color: " + item.color + " !important; opacity: 0.8 !important; }", 0);
+    sheet.insertRule("web-annotator[tag='" + item.name + "'] { background-color: " + item.color + " !important; opacity: 0.8 !important; }", 0);
 
-        /*        allTagAppliers[item.name] = rangy.createCssClassApplier("tag", {
-         elementTagName: "web-annotator",
-         elementProperties: {
-         title: item.name
-         },
-         elementAttributes: {
-         tag: item.name
-         }
-         });*/
-      });
-    }
-
-    $("#web-annotator-tags").html(options);
-  };
-
-  chrome.storage.sync.get("isActive", function(data){
-    isActive = data.isActive || false;
-  });
-
-  chrome.storage.sync.get("showPreview", function(data){
-    showPreview = data.showPreview || true;
-  });
-
-  chrome.storage.sync.get("isLinksActive", function(data){
-    isLinksActive = data.isLinksActive || true;
-  });
-
-  chrome.storage.sync.get("projects", function (data) {
-    var projects = data.projects || [];
-
-    // get the current project id after the projects have all been retrieved.
-    chrome.storage.sync.get("currentProjectId", function (currentProjData) {
-      var currentProjectId = currentProjData.currentProjectId;
-
-      if (currentProjectId != null) {
-        for (var i = 0; i < projects.length; i++) {
-          if (projects[i].id === currentProjectId) {
-            currentProject = projects[i];
-            break;
-          }
-        }
+    allTagAppliers[item.name] = rangy.createClassApplier("tag", {
+      elementTagName: "web-annotator",
+      elementProperties: {
+        title: item.name
+      },
+      elementAttributes: {
+        tag: item.name
       }
-
-      updateTags();
     });
   });
+};
 
-  chrome.storage.onChanged.addListener(function (changes) {
-    for (var key in changes) {
-      var storageChange = changes[key];
+var hideModal = function () {
+  $("#web-annotator-frame").css("display", "none");
+};
 
-      if (key === "isActive") {
-        isActive = storageChange.newValue;
-      } else if(key === "showPreview") {
-        showPreview = storageChange.newValue;
-      } else if (key === "isLinksActive") {
-        isLinksActive = storageChange.newValue;
-      } else if(key === "currentProjectId") {
-        console.log("content script: project changed!")
-      }
+var showModal = function () {
+  var top = Math.max($(window).height() / 2 - 152, 0);
+  var left = Math.max($(window).width() / 2 - 250, 0);
+
+  $("#web-annotator-frame").css({
+    display: "block",
+    position: 'fixed',
+    left: left + "px",
+    top: top + "px"
+  });
+};
+
+var tagCurrentText = function (tag) {
+
+  console.log("tagging current text with current tag: " + tag);
+
+  if (tag == null || tag == "") {
+    return;
+  }
+
+  if (allTagAppliers == null || allTagAppliers[tag] == null) {
+    throw Error("tag css class applier not found!");
+  }
+
+  allTagAppliers[tag].applyToSelection();
+};
+
+var handleMouseUp = function (e) {
+  if (hasBeenInit === false) {
+    return;
+  }
+
+  if (currentProject == null) {
+    return;
+  }
+
+  var selection = rangy.getSelection();
+  var selectionString = selection.toString();
+
+  if (selectionString.trim() == "") {
+    return;
+  }
+
+  chrome.storage.sync.get("isActive", function (data) {
+    if (data.isActive === true) {
+
+      chrome.storage.sync.get("showPreview", function (data) {
+        var showPreview = data.showPreview == null || data.showPreview === true
+
+        if (data.showPreview === true || currentTag == null || currentTag.trim() == "") {
+
+          // update the tags and text of the popup
+          var $webAnnotatorFrame = $("#web-annotator-frame");
+          if ($webAnnotatorFrame.length == 0) {
+            throw Error("Web Annotator Frame not loaded...");
+          }
+
+          $webAnnotatorFrame[0].contentWindow.postMessage({
+            tags: currentProject.tags,
+            currentTag: "",
+            selectedText: selectionString
+          }, extensionUrl);
+
+          showModal();
+        } else {
+          tagCurrentText(currentTag);
+        }
+      });
     }
+  });
+};
 
-    console.log("chrome storage updated in content script: " + JSON.stringify(changes));
+var initialize = function () {
+  if (hasBeenInit === true) {
+    return;
+  } else {
+    hasBeenInit = true;
+  }
+
+  rangy.init();
+
+  console.log("initializing web annotator: " + JSON.stringify(currentProject));
+
+  updateStyles();
+
+  var iframe = $("<iframe id='web-annotator-frame' style='width:500px; height: 304px; position: absolute; border:none; background-color: transparent; overflow: hidden; z-index: 99999; display:none;' frameborder='0' allowTransparency='true'></iframe>");
+  iframe.attr("src", chrome.extension.getURL('web-annotator-modal.html'));
+  $("body").append(iframe);
+
+  $("body").on("keyup", function (e) {
+    // hide the modal on escape
+    if (e.keyCode == 27) {
+      hideModal();
+    }
   });
 
-  chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
-    console.log("message recieved in content script: " + JSON.stringify(msg));
+  document.onmouseup = handleMouseUp;
+};
 
-    if (!msg.type) {
-      return;
-    }
+var deactivate = function () {
+  if (hasBeenInit === false) {
+    return;
+  }
 
-    if (msg.type === "report_back") {
+  console.log("deactivate web-annotator");
+};
+
+chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
+  console.log("message recieved in content script: " + JSON.stringify(msg));
+
+  if (!msg.type) {
+    return;
+  }
+
+  switch (msg.type) {
+    case "report_back":
       sendResponse(document.all[0].outerHTML);
-    } else if (msg.type === "update_project") {
-      currentProject = msg.data;
-      updateTags();
+      break;
+    case "deactivate":
+      currentProject = null;
+      deactivate();
+      break;
+    case "toggle_show_preview":
+      $("#web-annotator-show-preview").prop("checked", msg.showPreview);
+      break;
+    case "project_changed":
+      currentProject = msg.project;
+      updateStyles();
+      break;
+    case "activate":
+      currentProject = msg.project;
+      initialize();
+      break;
+    default:
+      console.log("unknown message: " + JSON.stringify(msg));
+      break;
+  }
+});
+
+// listen for all events from the iframe
+window.addEventListener('message', function (e) {
+  if (e.origin === extensionUrl) {
+    console.log("message recieved from new annotation modal: ");
+    console.log(e);
+
+    if (e.data.newAnnotation != null) {
+      currentTag = e.data.newAnnotation.tag;
+      tagCurrentText(e.data.newAnnotation.tag);
     }
-  });
 
-  $("#web-annotator-show-preview").on("change", function (e) {
-    var showPreview = e.originalEvent.target.checked;
-    chrome.storage.sync.set({showPreview: showPreview});
-  });
+    hideModal();
+  }
+}, false);
 
-  var tagCurrentText = function () {
-    var currentTag = $("#web-annotator-tags option:selected").text();
-    if (currentTag == null || currentTag == "") {
+chrome.storage.sync.get("isActive", function (data) {
+  if (data.isActive !== true) {
+    return;
+  }
+
+  chrome.storage.sync.get("currentProjectId", function (currentProjectData) {
+    if (currentProjectData.currentProjectId == null) {
       return;
     }
 
-    if (allTagAppliers[currentTag] != null) {
-      allTagAppliers[currentTag].applyToSelection();
-    }
-  };
-
-  $("#web-annotator-submit").on("click", function (e) {
-    e.preventDefault();
-    tagCurrentText();
-  });
-
-  $("#web-annotator-modal .close").on("click", function (e) {
-    e.preventDefault();
-    $("#web-annotator-modal").hide();
-  });
-
-  document.onmouseup = function (e) {
-    // we don't want to overwrite the target tag when selecting text in the modal popup
-    if ($(e.target).closest("#web-annotator-modal").length > 0) {
-      return;
-    }
-
-    var selection = rangy.getSelection();
-    var selectionString = selection.toString();
-
-    if (selectionString.trim() == "") {
-      return;
-    }
-
-    chrome.storage.sync.get("isActive", function (data) {
-      var isActive = data.isActive || false;
-
-      if (isActive === false) {
+    chrome.storage.sync.get("projects", function (projectsData) {
+      if (projectsData.projects == null) {
         return;
       }
 
-      if (hasBeenInit === false) {
-        createModal();
-        hasBeenInit = true;
-      }
+      for (var i = 0; i < projectsData.projects.length; i++) {
 
-      chrome.storage.sync.get("showPreview", function (data) {
-        var showPreview = data.showPreview || true;
-
-        if (showPreview) {
-          //$("#web-annotator-show-preview").prop("checked", showPreview);
-          document.getElementById("web-annotator-input").value = selectionString;
-          $("#web-annotator-modal").show();
-        } else {
-          tagCurrentText();
+        if (projectsData.projects[i].id === currentProjectData.currentProjectId) {
+          currentProject = projectsData.projects[i];
+          initialize();
+          break;
         }
-      });
+      }
     });
-  };
-})();
+  });
+});
