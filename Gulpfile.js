3c4e7333e7715ac6c50ad74079946619c69bfade
Brian McCarthy <brian.mcc@rthy.org>
2015-01-01 22:27:29 -0500

    updated build process

--- Gulpfile.js
+++ Gulpfile.js
@@ -8,7 +8,20 @@ var gulp = require('gulp'),
   uglify = require('gulp-uglify'),
   zip = require('gulp-zip'),
   bump = require('gulp-bump'),
-  del = require('del');
+  del = require('del'),
+  usemin = require('gulp-usemin');
+
+var appFolder = './app/';
+
+gulp.task('usemin', function () {
+  return gulp.src(appFolder + '*.html')
+    .pipe(usemin({
+      css: [minifycss({root: appFolder + 'styles', keepSpecialComments: 0}), 'concat'],
+      js: [stripdebug(), uglify({outSourceMap: true}), 'concat'],
+      html: [cleanhtml()]
+    }))
+    .pipe(gulp.dest('build'));
+});
 
 //clean build directory
 gulp.task('clean', function (cb) {
@@ -23,25 +36,18 @@ gulp.task('bump', function () {
     .pipe(bump())
     .pipe(gulp.dest('./'));
 
-  return gulp.src('app/manifest.json')
+  return gulp.src(appFolder + 'manifest.json')
     .pipe(bump())
     .pipe(gulp.dest('app'));
 });
 
 //copy static folders to build directory
-gulp.task('copy', function () {
-  gulp.src('app/icons/**')
+gulp.task('copy', ['bump'], function () {
+  gulp.src(appFolder + 'icons/**')
     .pipe(gulp.dest('build/icons'));
-  gulp.src('app/_locales/**')
+  gulp.src(appFolder + '_locales/**')
     .pipe(gulp.dest('build/_locales'));
-  return gulp.src('app/manifest.json')
-    .pipe(gulp.dest('build'));
-});
-
-//copy and compress HTML files
-gulp.task('html', function () {
-  return gulp.src('app/*.html')
-    .pipe(cleanhtml())
+  return gulp.src(appFolder + 'manifest.json')
     .pipe(gulp.dest('build'));
 });
 
@@ -52,44 +58,54 @@ gulp.task('jshint', function () {
     .pipe(jshint.reporter('default'));
 });
 
-//copy vendor scripts and uglify all other scripts, creating source maps
-gulp.task('scripts', ['jshint'], function () {
-  gulp.src('app/bower_components/angular/angular.*js')
-    .pipe(gulp.dest('build/bower_components/angular'));
-  gulp.src('app/bower_components/angular-bootstrap-colorpicker/{css,img,js}/**')
-    .pipe(gulp.dest('build/bower_components/angular-bootstrap-colorpicker'));
-  gulp.src('app/bower_components/bootstrap/dist/**')
-    .pipe(gulp.dest('build/bower_components/bootstrap/dist'));
-  gulp.src('app/bower_components/jquery/dist/**')
-    .pipe(gulp.dest('build/bower_components/jquery/dist'));
-  gulp.src('app/bower_components/rangy-official/*.js')
-    .pipe(gulp.dest('build/bower_components/rangy-official'));
+gulp.task('chromeManifest', function () {
+  var manifest = require(appFolder + 'manifest');
 
-  return gulp.src('app/scripts/**/*.js')
+  var backgroundScripts = manifest.background.scripts;
+  backgroundScripts.push('!bower_components/**');
+
+  gulp.src(backgroundScripts, {cwd: appFolder})
     .pipe(stripdebug())
-    .pipe(uglify({outSourceMap: true}))
+    .pipe(uglify())
     .pipe(gulp.dest('build/scripts'));
+
+  for (var i = 0; i < manifest.content_scripts.length; i++) { // jshint ignore:line
+    var contentScripts = manifest.content_scripts[i].js || []; // jshint ignore:line
+    contentScripts.push('!bower_components/**');
+
+    gulp.src(contentScripts, {cwd: appFolder})
+      .pipe(stripdebug())
+      .pipe(uglify())
+      .pipe(gulp.dest('build/scripts'));
+  }
 });
 
-//minify styles
-gulp.task('styles', function () {
-  return gulp.src('app/styles/**/*.css')
-    .pipe(minifycss({root: 'app/styles', keepSpecialComments: 0}))
-    .pipe(gulp.dest('build/styles'));
+//copy vendor scripts
+gulp.task('scripts', ['jshint'], function () {
+  gulp.src('app/bower_components/angular/*.min.js')
+    .pipe(gulp.dest('build/bower_components/angular'));
+  gulp.src('app/bower_components/angular-bootstrap-colorpicker/**/{*.min.css,*.png,*.min.js}')
+    .pipe(gulp.dest('build/bower_components/angular-bootstrap-colorpicker'));
+
+  gulp.src('app/bower_components/bootstrap/dist/fonts/**')
+    .pipe(gulp.dest('build/bower_components/bootstrap/dist/fonts'));
+  gulp.src('app/bower_components/bootstrap/dist/**/{*.min.css,*.min.js}')
+    .pipe(gulp.dest('build/bower_components/bootstrap/dist'));
+
+  gulp.src('app/bower_components/jquery/dist/*.min.js')
+    .pipe(gulp.dest('build/bower_components/jquery/dist'));
+
+  return gulp.src('app/bower_components/rangy-official/*.min.js')
+    .pipe(gulp.dest('build/bower_components/rangy-official'));
 });
 
 //build ditributable and sourcemaps after other tasks completed
-gulp.task('zip', ['bump', 'html', 'scripts', 'styles', 'copy'], function () {
-  var manifest = require('./app/manifest'),
-    pkg = require('./package.json'),
-    distFileName = pkg.name + '-v' + manifest.version + '.zip',
-    mapFileName = pkg.name + '-v' + manifest.version + '-maps.zip';
-  //collect all source maps
-  gulp.src('build/scripts/**/*.map')
-    .pipe(zip(mapFileName))
-    .pipe(gulp.dest('dist'));
-  //build distributable extension
-  return gulp.src(['build/**', '!build/scripts/**/*.map'])
+gulp.task('zip', ['scripts', 'usemin', 'copy', 'chromeManifest'], function () {
+  var pkg = require('./package.json'),
+    manifest = require('./build/manifest.json'),
+    distFileName = pkg.name + '-v' + manifest.version + '.zip';
+
+  return gulp.src(['build/**'])
     .pipe(zip(distFileName))
     .pipe(gulp.dest('dist'));
 });
